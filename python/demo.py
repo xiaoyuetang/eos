10d76e8ca6112f3bba1b7e01201a72a5a6766a84
keelia <xiaoyuetang2017@gmail.com>
2020-03-08 23:15:46 +0800

    - add ibug dataset

--- python/demo.py
+++ python/demo.py
@@ -1,23 +1,24 @@
 import eos
+import trimesh
 import numpy as np
 
 def main():
     """Demo for running the eos fitting from Python."""
-    landmarks = read_pts('../bin/data/image_0010.pts')
+    landmarks = read_pts('ibug/image_003_1.pts')
     image_width = 1280 # Make sure to adjust these when using your own images!
     image_height = 1024
 
-    model = eos.morphablemodel.load_model("../share/sfm_shape_3448.bin")
-    blendshapes = eos.morphablemodel.load_blendshapes("../share/expression_blendshapes_3448.bin")
+    model = eos.morphablemodel.load_model("share/sfm_shape_3448.bin")
+    blendshapes = eos.morphablemodel.load_blendshapes("share/expression_blendshapes_3448.bin")
     # Create a MorphableModel with expressions from the loaded neutral model and blendshapes:
     morphablemodel_with_expressions = eos.morphablemodel.MorphableModel(model.get_shape_model(), blendshapes,
                                                                         color_model=eos.morphablemodel.PcaModel(),
                                                                         vertex_definitions=None,
                                                                         texture_coordinates=model.get_texture_coordinates())
-    landmark_mapper = eos.core.LandmarkMapper('../share/ibug_to_sfm.txt')
-    edge_topology = eos.morphablemodel.load_edge_topology('../share/sfm_3448_edge_topology.json')
-    contour_landmarks = eos.fitting.ContourLandmarks.load('../share/ibug_to_sfm.txt')
-    model_contour = eos.fitting.ModelContour.load('../share/sfm_model_contours.json')
+    landmark_mapper = eos.core.LandmarkMapper('share/ibug_to_sfm.txt')
+    edge_topology = eos.morphablemodel.load_edge_topology('share/sfm_3448_edge_topology.json')
+    contour_landmarks = eos.fitting.ContourLandmarks.load('share/ibug_to_sfm.txt')
+    model_contour = eos.fitting.ModelContour.load('share/sfm_model_contours.json')
 
     (mesh, pose, shape_coeffs, blendshape_coeffs) = eos.fitting.fit_shape_and_pose(morphablemodel_with_expressions,
         landmarks, landmark_mapper, image_width, image_height, edge_topology, contour_landmarks, model_contour)
@@ -27,8 +28,12 @@ def main():
 
     # Or for example extract the texture map, like this:
     # import cv2
-    # image = cv2.imread('../bin/data/image_0010.png')
+    # image = cv2.imread('ibug/image_003_1.jpg')
     # isomap = eos.render.extract_texture(mesh, pose, image)
+
+    # help(eos)
+    fitted_mesh = eos.render.render(pose)
+    print (fitted_mesh)
 
 
 def read_pts(filename):
